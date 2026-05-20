ef50d345c47a665293ec82c009f56f547e1eb64f
masrobi <semut.gondrong@gmail.com>
2026-05-20 10:46:23 +0700

    Add files via upload

---
+++ js/application.js
@@ -0,0 +1,20 @@
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "Restaurant",
+    "name": "Sate Kambing Turbo",
+    "image": "https://masrobi.github.io/katalog-wa/img/hero4.png",
+    "description": "Restoran sate kambing premium dengan menu gulai, tongseng, dan tengkleng. Bisa pesan online via WhatsApp.",
+    "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Kota Anda",
+        "addressRegion": "Provinsi",
+        "addressCountry": "ID"
+    },
+    "priceRange": "Rp25.000 - Rp100.000",
+    "servesCuisine": "Indonesian, Satay, Halal",
+    "telephone": "+6289652257995",
+    "url": "https://masrobi.github.io/katalog-wa/",
+    "orderType": "https://schema.org/Online"
+}
+</script>
